1a03891c49e1f8c1b3ae30926625a1abc95186e2
goldlilya1612 <goldlilya1612@yandex.ru>
2023-12-23 20:22:17 +0300

    [feat]: create chatsBlock

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
-export { FormAuth } from './form-auth'
-export { Input } from './input'
-export { Button } from './button'
-export { RedirectLink } from './redirect-link'
-export { ChatsBlock } from './chats-block'
-export { SearchField } from './search-field'
+export { FormAuth } from './form-auth';
+export { Input } from './input';
+export { Button } from './button';
+export { RedirectLink } from './redirect-link';
+export { ChatsBlock } from './chats-block';
+export { SearchField } from './search-field';
+export { ChatCard } from './chat-card';
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,85 @@ import Handlebars from 'handlebars';
 import * as Components from './components';
 import * as Pages from './pages';
 
+const props = {
+    chats: [
+        {
+            name:  "Андрей",
+            message: "Изображение",
+            isIncoming: true,
+            time: '10:49',
+            count: 2,
+        },
+        {
+            name:  "Киноклуб",
+            message: "Круто!",
+            isIncoming: false,
+            time: '12:00',
+        },
+        {
+            name:  "Илья",
+            message: "Друзья, у меня для вас особенный выпуск новостей! Друзья, у меня для вас особенный выпуск новостей!",
+            isIncoming: true,
+            time: '13:23',
+            count: 4,
+        },
+        {
+            name:  "Андрей",
+            message: "Изображение",
+            isIncoming: true,
+            time: 'Пн',
+            count: 2,
+        },
+        {
+            name:  "Киноклуб",
+            message: "Круто!",
+            isIncoming: false,
+            time: 'Вт',
+        },
+        {
+            name:  "Илья",
+            message: "Друзья, у меня для вас особенный выпуск новостей! Друзья, у меня для вас особенный выпуск новостей!",
+            isIncoming: true,
+            time: 'Ср',
+            count: 4,
+        },
+        {
+            name:  "Андрей",
+            message: "Изображение",
+            isIncoming: true,
+            time: 'Чт',
+            count: 2,
+        },
+        {
+            name:  "Киноклуб",
+            message: "Круто!",
+            isIncoming: false,
+            time: 'Пт',
+        },
+        {
+            name:  "Илья",
+            message: "Друзья, у меня для вас особенный выпуск новостей! Друзья, у меня для вас особенный выпуск новостей!",
+            isIncoming: true,
+            time: 'Сб',
+            count: 4,
+        },
+    ],
+}
+
+
 const pages = {
     'login': [Pages.LoginPage, {title: 'Вход'}],
     'register': [ Pages.RegisterPage, {title: 'Регистрация'} ],
-    'chats': [Pages.ChatsPage]
+    'chats': [Pages.ChatsPage, props]
 }
 
 Object.entries(Components).forEach(([ name, component ]) => {
     Handlebars.registerPartial(name, component);
+    Handlebars.registerHelper('isEmpty', function (value) {
+        console.log(value === '')
+        return value === '';
+    });
+
 });
 
 function navigate(page: string) {
