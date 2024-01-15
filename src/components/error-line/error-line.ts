a3f9e9c6f976b9c7586041c94bc4fee5e9f8f834
goldlilya1612 <goldlilya1612@yandex.ru>
2024-01-15 21:36:12 +0300

    [feat]: - add constructors

--- src/components/error-line/error-line.ts
+++ src/components/error-line/error-line.ts
@@ -1,6 +1,13 @@
 import Block from '../../utils/core/Block';
 
+interface IErrorLineProps {
+  error: string
+}
 export class ErrorLine extends Block {
+  constructor(props: IErrorLineProps) {
+    super(props);
+  }
+
   protected render(): string {
     return (`
             <span class="input__error">{{error}}</span>
